bcef396a899a679de3b373c232908a82a3ba7ab1
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2023-03-21 13:23:44 +0100

    Change measures against duplicated detection

--- src/CodeMaker.java
+++ src/CodeMaker.java
@@ -6,53 +6,58 @@ import javax.swing.JLabel;
 public class CodeMaker extends ColorCode {
     private ArrayList<Double> curFitnessSeq;
     // Keep track of CodeMaker data
-    private ArrayList<Color> colorsList;
-    private ArrayList<Integer> colorsNum;
+    // private ArrayList<Color> colorsList;
+    // private ArrayList<Integer> colorsNum;
+
+    private ArrayList<Boolean> cellsAccessList;
 
     public CodeMaker(int length, String title, int cellsSize) {
         super(length, title, cellsSize);
-        this.colorsList = new ArrayList<>();
-        this.colorsNum = new ArrayList<>();
+        // this.colorsList = new ArrayList<>();
+        // this.colorsNum = new ArrayList<>();
+
+        this.cellsAccessList = new ArrayList<>();
+        for (int i = 0; i < this.getLength(); i++) this.cellsAccessList.add(true);
     }
 
     public void launch() {
         for (int i = 0; i < this.getLength(); i++) {
             Color randColor = ColorWheel.getRandColor();
             this.updateCellColor(i, randColor);
-            this.arrangeColorsData(randColor);
+            // this.arrangeColorsData(randColor);
         }
     }
 
-    public void arrangeColorsData(Color color) {
-        for (int i = 0; i < this.colorsList.size(); i++) {
-            if (this.colorsList.get(i) == color) {
-                this.colorsNum.set(i, this.colorsNum.get(i) + 1);
-                return;
-            }
-        }
+    // public void arrangeColorsData(Color color) {
+    //     for (int i = 0; i < this.colorsList.size(); i++) {
+    //         if (this.colorsList.get(i) == color) {
+    //             this.colorsNum.set(i, this.colorsNum.get(i) + 1);
+    //             return;
+    //         }
+    //     }
 
-        this.colorsList.add(color);
-        this.colorsNum.add(1);
-    }
+    //     this.colorsList.add(color);
+    //     this.colorsNum.add(1);
+    // }
 
-    public void subColorUnits(Color color) {
-        for (int i = 0; i < this.colorsList.size(); i++) {
-            if (this.colorsList.get(i) == color) {
-                this.colorsNum.set(i, this.colorsNum.get(i) - 1);
-                return;
-            }
-        }
-    }
+    // public void subColorUnits(Color color) {
+    //     for (int i = 0; i < this.colorsList.size(); i++) {
+    //         if (this.colorsList.get(i) == color) {
+    //             this.colorsNum.set(i, this.colorsNum.get(i) - 1);
+    //             return;
+    //         }
+    //     }
+    // }
 
-    public int getColorUnitLeft(Color color) {
-        for (int i = 0; i < this.colorsList.size(); i++) {
-            if (this.colorsList.get(i) == color) {
-                return this.colorsNum.get(i);
-            }
-        }
+    // public int getColorUnitLeft(Color color) {
+    //     for (int i = 0; i < this.colorsList.size(); i++) {
+    //         if (this.colorsList.get(i) == color) {
+    //             return this.colorsNum.get(i);
+    //         }
+    //     }
 
-        return 0;
-    }
+    //     return 0;
+    // }
 
     public boolean isEqual(ColorCode colorCode) {
         boolean equal = true;
@@ -76,6 +81,8 @@ public class CodeMaker extends ColorCode {
         ArrayList<Double> fitnessSeq = new ArrayList<>();
         ArrayList<JLabel> cbColorSeq = colorCode.getColorSeq();
 
+        for (int i = 0; i < this.getLength(); i++) this.cellsAccessList.set(i, true);
+
         for (int i = 0; i < this.getLength(); i++) {
             double fitness = 0;
 
@@ -96,7 +103,7 @@ public class CodeMaker extends ColorCode {
                 for (Integer index : indexes) {
                     if (index == i) {
                         fitness += 0.5;
-                        subColorUnits(curColor);
+                        // subColorUnits(curColor);
                         break;
                     }
                 }
@@ -113,9 +120,12 @@ public class CodeMaker extends ColorCode {
     public boolean colorExists(Color color) {
         boolean exist = false;
 
+        // if (color == this.getColorSeq().get(i).getBackground() && this.getColorUnitLeft(color) > 0 && this.cellsAccessList.get(i)) 
+
         for (int i = 0; i < this.getLength(); i++) {
-            if (color == this.getColorSeq().get(i).getBackground() && this.getColorUnitLeft(color) > 0) {
+            if (color == this.getColorSeq().get(i).getBackground() && this.cellsAccessList.get(i)) {
                 exist = true;
+                this.cellsAccessList.set(i, false);
                 break;
             }
         }
